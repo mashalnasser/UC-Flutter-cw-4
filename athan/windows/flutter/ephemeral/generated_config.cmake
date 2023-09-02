da81170f0586ac755727747303253586f547dfa0
mashalnasser <mmm760778@gmail.com>
2023-09-02 20:05:00 +0300

    updated

---
+++ athan/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\abd72\\OneDrive\\سطح المكتب\\الكويت تبرمج فلاتر 3\\athan" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\abd72\\OneDrive\\سطح المكتب\\الكويت تبرمج فلاتر 3\\athan"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\abd72\\OneDrive\\سطح المكتب\\الكويت تبرمج فلاتر 3\\athan\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\abd72\\OneDrive\\سطح المكتب\\الكويت تبرمج فلاتر 3\\athan"
+  "FLUTTER_TARGET=C:\\Users\\abd72\\OneDrive\\سطح المكتب\\الكويت تبرمج فلاتر 3\\athan\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8xYWM2MTFjNjRlYWRiZDkzYzVmNWFiYTU0OTRiOGZjM2IzNWVlOTUyLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\abd72\\OneDrive\\سطح المكتب\\الكويت تبرمج فلاتر 3\\athan\\.dart_tool\\package_config.json"
+)
